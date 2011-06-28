10a16dc0425f2dcfba8baff0640c9f83017be784
Mario Behrendt <mario.behrendt@netresearch.de>
2011-06-28 09:14:54 +0200

    Hide page in relation select to prevent self-relation

--- app/code/local/DirtyDozen/CmsRelations/Model/Mysql4/Page/Collection.php
+++ app/code/local/DirtyDozen/CmsRelations/Model/Mysql4/Page/Collection.php
@@ -9,12 +9,17 @@ class DirtyDozen_CmsRelations_Model_Mysql4_Page_Collection extends Mage_Cms_Mode
      */
     public function toOptionIdArray()
     {
+        $id = Mage::app()->getFrontController()->getRequest()->getParam('page_id');
+
         $res = array();
         foreach ($this as $item) {
             $identifier = $item->getData('page_id');
-            $data['value'] = $identifier;
-            $data['label'] = $item->getData('title');
-            $res[] = $data;
+
+            if ($id != $identifier) {
+                $data['value'] = $identifier;
+                $data['label'] = $item->getData('title');
+                $res[] = $data;
+            }
         }
 
         return $res;
